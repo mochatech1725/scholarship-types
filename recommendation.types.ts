bc6a7dee1745d6537f89210db23089496099bcce
TeialD <teial.starks@gmail.com>
2025-10-31 10:18:15 -0700

    make sure due_date accepts null

--- recommendation.types.ts
+++ recommendation.types.ts
@@ -5,7 +5,7 @@ export interface Recommendation {
   application_id: number;
   recommender_id: number;
   submitted_at?: Date | null;
-  due_date?: Date;
+  due_date?: Date | null;
   status: TRecommendationStatus;
   created_at?: Date | null;
   updated_at?: Date | null;
